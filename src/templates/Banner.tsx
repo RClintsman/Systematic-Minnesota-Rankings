30210e23bd219754a436f3a57898935158e0515d
merzk001 <merzk001@umn.edu>
2024-01-21 17:43:39 -0600

    Changes to the template

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link';
 
 import { Button } from '../button/Button';
-import { CTABanner } from '../cta/CTABanner';
 import { Section } from '../layout/Section';
 
 const Banner = () => (
   <Section>
-    <CTABanner
+    {/* <CTABanner
       title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
       subtitle="Start your Free Trial."
       button={
@@ -14,7 +13,11 @@ const Banner = () => (
           <Button>Get Started</Button>
         </Link>
       }
-    />
+    /> */}
+    {/* change later */}
+    <Link href="https://github.com/RClintsman/Systematic-Minnesota-Rankings/tree/TarikDev">
+      <Button>Github</Button>
+    </Link>
   </Section>
 );
 
